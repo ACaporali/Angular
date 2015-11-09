3034cc426f1dac52a2bdd89215e90a87b020af7e
Alice Caporali <Alice.Caporali@e.ujf-grenoble.fr>
2015-11-09 17:43:30 +0100

    Sélection d'un élément dans un liste fonctionnel + affichage des détails de l'élément selectionné. Ajout d'un élément dans la liste avec des paramètres d'altitude et de longitude fonctionnel

--- src/assets/js/controllers/checkin.js
+++ src/assets/js/controllers/checkin.js
@@ -13,9 +13,53 @@
 			console.log(response.data);
 			$scope.checkins = response.data;
 		}, function errorCallback(response){
-			//console.log(response);
+			console.log(response);
 		});
 		
+	})
+
+	.controller('checkinDetailsController', function($routeParams, $http, $scope){
+		console.log("lala");
+		console.log($routeParams);
+		$http({
+			method: 'GET',
+			url: 'http://checkin-api.dev.cap-liberte.com/checkin/'+$routeParams.checkinId+''
+
+		}).then(function successCallback(response){
+			console.log(response.data);
+			$scope.checkins = response.data;
+		}, function errorCallback(response){
+			//console.log(response);
+		});
+
+	})
+
+		
+	.controller('checkinFormController', function($scope, $http){
+		$scope.submit = function(){
+			console.log($scope.lat + ' ' + $scope.lng);
+
+			$http({
+				method: 'POST',
+				url: 'http://checkin-api.dev.cap-liberte.com/checkin',
+				data:{
+					lat: $scope.lat,
+					lng: $scope.lng
+				},
+				headers:{
+					'Content-Type': undefined
+
+				}
+
+			}).then(function successCallback(response){
+				console.log(response.data);
+				$scope.checkins = response.data;
+			}, function errorCallback(response){
+				//console.log(response);
+			});
+
+
+		};
 	});
 		
 
