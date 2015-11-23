b8ce37e5fb47905c6f4c53834bdf69a119361e98
Alice Caporali <Alice.Caporali@e.ujf-grenoble.fr>
2015-11-23 15:31:17 +0100

    Mise en place de Cordova + mise en place du fichier www + debut de la mise en place d'un simulateur IOS

--- gulpfile.js
+++ gulpfile.js
@@ -1,9 +1,22 @@
 var gulp        = require('gulp');
 var browserSync = require('browser-sync').create();
+var useref = require('gulp-useref');
 
 // Static server
 gulp.task('serve', function() {
     browserSync.init({
         server: "./src"
     });
+});
+
+gulp.task('copy-angular-templates', function(done){
+	gulp.src(['src/assets/template/**/*'])
+		.pipe(gulp.dest('www/assets/template/'))
+		.on('end', done);
+});
+
+gulp.task('make', ['copy-angular-templates'], function(){
+	return gulp.src('src/*.html')
+		.pipe(useref())
+		.pipe(gulp.dest('www'));
 });
