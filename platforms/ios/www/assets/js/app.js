ed404e0dbb2bbf5a465899198ef4566a390e9c71
alice38 <petitkaporal@hotmail.fr>
2016-01-30 13:35:31 +0100

    ajout des fichiers (assets) pour le bon fonctionnement de cordova dans le dossier www

---
+++ platforms/ios/www/assets/js/app.js
@@ -0,0 +1,62 @@
+(function(angular){
+	'use strict';
+
+	var myApp = angular.module('myApp',[
+		'ngRoute', 'LocalStorageModule',
+		'helloModule','contactModule','checkinModule'
+	])
+
+	
+	myApp.config(function($routeProvider){
+		$routeProvider
+		.when('/',{
+			templateUrl: 'assets/template/checkinList.html',
+			})
+		.when('/checkin/:checkinId',{
+			templateUrl: 'assets/template/checkinDetails.html',
+			// controller: 'checkinDetailsController'
+		});
+	});
+
+	myApp.directive('myMaps', function(){
+        return{
+        	scope :{
+        		lat: '@',
+        		lon: '@' 	
+          },
+          restrict: 'E',
+          template: '<div></div>',
+          replace: true,
+          link: function(scope, element, attrs){
+            //scope.$watch permet de voir si lat change. 
+            //Le html s'execute avant la requete que renvoie lat et lon (openweathermap dans checkinDetailsController) donc lat et lon (dans la html) sont null 
+            //Si c'est la cas, appel de la fonction map();
+            scope.$watch('lat', function() {
+              map();
+            });
+
+            var map = function(){
+              scope.lat = scope.lat; 
+              scope.lon = scope.lon; 
+              var myLatLng = new google.maps.LatLng(scope.lat, scope.lon);
+             
+              var mapOption = {
+                center : myLatLng,
+                zoom: 13,
+                mapTypeId: google.maps.MapTypeId.TERRAIN
+              };
+              //var map = new google.maps.Map(document.getElementById(attrs.id), mapOption);
+              var map = new google.maps.Map(document.getElementById("map-canvas"), mapOption);
+              // Avec une ligne ou l'autre ça fonctionnes
+
+              var marker = new google.maps.Marker({
+                position: myLatLng,
+                title:"Ma position"
+              });
+              marker.setMap(map);
+            }
+          }
+        }
+      });
+
+})(window.angular);
